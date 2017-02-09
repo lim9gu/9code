74e1bb228c6ae252a01e0f7a1770540117b549a4
G.Lim <lim9gu@gmail.com>
2017-02-09 20:39:37 +0900

    Update header_obj_squean.py

--- header_obj_squean.py
+++ header_obj_squean.py
@@ -6,7 +6,12 @@ import numpy as np
 from astropy.io import fits
 
 folder = str(sys.argv[1]) # directory name YYYYMMDD or GRBXXXXXXA.
-object = str(sys.argv[2]) # Enter object name which you are looking for. Try other names like M51, M51a, Messier51, messier51...
+header_name = str(sys.argv[2]) # Choose in object, filter, exptime, imagetype.
+object = str(sys.argv[3]) # Enter header name which you are looking for. 
+
+# object : Try other names like M51, M51a, Messier51, messier51...
+# filter : U, B, V, R, I , u, g, r, i, z ...
+# exptime 
 
 os.chdir(folder)
 os.system('ls *.fits > inim.list')
@@ -14,4 +19,8 @@ os.system('ls *.fits > inim.list')
 inim = np.genfromtxt('inim.list', dtype = str)
 
 for i = 0 in range(len(inim)) :
-  
+  hdr = fits.getheader(inim[i])
+  objname = hdr['object']
+  filter = hdr['filter']
+  exptime = hdr['exptime']
+  imagetype = hdr['']
