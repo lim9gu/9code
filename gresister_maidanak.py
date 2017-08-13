836cdea6206f0989ac5e44aa75101ee7874d6e6d
G.Lim <lim9gu@gmail.com>
2017-08-13 22:10:00 +0900

    Update gresister_maidanak.py

--- gresister_maidanak.py
+++ gresister_maidanak.py
@@ -1,1 +1,34 @@
-import pyraf
+# Code for running gresister IRAF to Maidanak.
+# Correct the distortion of WCS coordinate.
+# 2017-08-13 G.Lim created.
+
+from pyraf import iraf
+import numpy as np
+import os, sys
+
+'''
+gregister input output database transforms
+
+(input)
+List of images to be transformed.
+
+(output)
+List of output images.
+
+(database)
+The name of the text file database produced by GEOMAP 
+containing the coordinate transformation(s).
+
+(transforms)
+The list of the database record(s) containing the transformations. 
+The number of transforms must be 1 or the same as the number of input images. 
+Transforms is usually the name of the text file input to GEOMAP 
+which lists the reference and input coordinates of the control points.
+'''
+os.system('ls ac*.fits > im.list')
+
+
+
+
+
+os.system('rm im.list')
